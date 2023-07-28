ccb1f2108e23b435efe659da2aa5c646a7f4050c
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-07-28 23:18:09 +0300

    Getting multiple news sources.

--- index.js
+++ index.js
@@ -5,9 +5,42 @@ const cheerio = require('cheerio')
 
 const app = express(); //Create an instance of the app
 
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ""
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis/all',
+        base: ''
+    }, 
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: "https://www.telegraph.co.uk"
+    }
+]
+
 const articles = []
 
-const { resolve } = require('url'); // Import the 'resolve' function from the 'url' module
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then((response) => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")').each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+
+            articles.push({ title, url: newspaper.base + url , source: newspaper.name })
+        })
+
+
+    })
+})
 
 
 app.get('/', (req, res) => {
@@ -15,30 +48,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis/all')
-    .then((response) => {
-        const html = response.data
-        const baseUrl = 'https://www.theguardian.com'; // Base URL of the web page
-        const $ = cheerio.load(html)
-
-        $('a[href*=climate]').each(function () {
-            // const h3Element = $(this).find('h3'); //Find the <h3> element within the current anchor tag
-            const title = $(this).text(); // Extract the text content of the <h3> element
-            const href = $(this).attr('href')
-
-             // Convert the relative URL to an absolute URL
-             const url = resolve(baseUrl, href);
-            //  if(title.toLowerCase().includes("climate")){
-            //     articles.push({ title, url })
-            //  }
-
-            articles.push({ title, url })
-        })
-        res.json(articles)
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+    res.json(articles)
 })
+
+
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
